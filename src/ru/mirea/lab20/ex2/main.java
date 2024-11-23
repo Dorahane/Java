04182e46ffd322e5e7e6d7ca7703fe31a4462dd9
Dorahane <100021201+Dorahane@users.noreply.github.com>
2024-11-23 15:34:14 +0300

    Delete src/ru/mirea/lab20 directory

--- src/ru/mirea/lab20/ex2/main.java
+++
@@ -1,8 +0,0 @@
-package lab20.ex2;
-
-public class main {
-    public static void main(String[] args) {
-        ex1<String, Integer, Double> obj = new ex1<>("Hello", 42, 3.14);
-        obj.printCNames();
-    }
-}
